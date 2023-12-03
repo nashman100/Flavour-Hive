4e266d20bac90bc5aa93611d011f5584311ec7c1
zoedetlefsen <122552548+zoedetlefsen@users.noreply.github.com>
2023-12-02 20:02:58 -0500

    updated navigation layout and activity. Implemented Google Cloud API - still getting error - will continue to work on it

--- app/src/main/java/com/example/flavourhive/NavigationActivity.java
+++ app/src/main/java/com/example/flavourhive/NavigationActivity.java
@@ -1,51 +1,144 @@
 package com.example.flavourhive;
 
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
+import android.text.InputType;
+import android.util.Log;
+import android.view.inputmethod.EditorInfo;
+import android.widget.SearchView;
+import android.widget.TextView;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.gms.maps.SupportMapFragment;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class NavigationActivity extends AppCompatActivity {
 
-    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1001;
-    private ImageButton map_button;
+    private SearchView searchView;
+    private TextView findTextView;
+    private SupportMapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.navigation_layout);
 
-        map_button = findViewById(R.id.map_button);
-        map_button.setOnClickListener(new View.OnClickListener() {
+        searchView = findViewById(R.id.searchView);
+        findTextView = findViewById(R.id.FindTextView);
+
+        // Set up SearchView
+        searchView.setImeOptions(EditorInfo.IME_ACTION_SEARCH);
+        searchView.setInputType(InputType.TYPE_CLASS_TEXT);
+        searchView.setQueryHint("Enter location");
+
+        // Set up OnQueryTextListener
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onClick(View view) {
-                // Check if the app has the necessary permissions
-                if (ContextCompat.checkSelfPermission(NavigationActivity.this,
-                        android.Manifest.permission.ACCESS_FINE_LOCATION) !=
-                        PackageManager.PERMISSION_GRANTED) {
-                    // Request the permission
-                    ActivityCompat.requestPermissions(NavigationActivity.this,
-                            new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                            LOCATION_PERMISSION_REQUEST_CODE);
-                    return;
-                }
+            public boolean onQueryTextSubmit(String query) {
+                // Handle the search here, e.g., initiate the PlacesTask
+                new PlacesTask().execute(query);
+                return true;
+            }
 
-                // Create an intent
-                Uri uri = Uri.parse("geo:43.6759374,-79.4111022?q=restaurants");
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-
-                // Check if there's an app available to handle the intent
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    Intent chooser = Intent.createChooser(intent, "How should I open this location?");
-                    startActivity(chooser);
-                }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // Handle text changes if needed
+                return false;
             }
         });
+
+        // Load Google Maps Fragment
+        loadMapFragment();
+    }
+
+
+    private void loadMapFragment() {
+        mapFragment = new SupportMapFragment();
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.mapFragment, mapFragment);
+        transaction.commit();
+    }
+
+    private class PlacesTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String query = params[0];
+            String apiKey = "AIzaSyC55FD2E_Ua7KEB97cQFn9nPowKhfkuiCI";
+
+            try {
+                // Encode the query to be used in the URL
+                String encodedQuery = java.net.URLEncoder.encode(query, "UTF-8");
+
+                // Create the URL for the textsearch endpoint
+                URL url = new URL("https://maps.googleapis.com/maps/api/place/textsearch/json" +
+                        "?query=" + encodedQuery +
+                        "&key=" + apiKey);
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                try {
+                    InputStream stream = new BufferedInputStream(connection.getInputStream());
+                    return convertStreamToString(stream);
+                } finally {
+                    connection.disconnect();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                try {
+                    JSONObject jsonObject = new JSONObject(result);
+
+                    // Check for errors in the API response
+                    if (jsonObject.has("error_message")) {
+                        String errorMessage = jsonObject.getString("error_message");
+                        Log.e("PlacesTask", "API Error: " + errorMessage);
+                        return;
+                    }
+
+                    JSONArray results = jsonObject.getJSONArray("results");
+
+                    for (int i = 0; i < results.length(); i++) {
+                        JSONObject place = results.getJSONObject(i);
+
+                        // Extract information about the place
+                        String name = place.getString("name");
+                        String address = place.getString("formatted_address");
+
+                        // Log the information (you can modify this part based on your requirements)
+                        Log.d("PlacesTask", "Place #" + (i + 1) + ":");
+                        Log.d("PlacesTask", "Name: " + name);
+                        Log.d("PlacesTask", "Address: " + address);
+                        Log.d("PlacesTask", "---------------------");
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        private String convertStreamToString(InputStream stream) {
+            java.util.Scanner s = new java.util.Scanner(stream).useDelimiter("\\A");
+            return s.hasNext() ? s.next() : "";
+        }
     }
 }
